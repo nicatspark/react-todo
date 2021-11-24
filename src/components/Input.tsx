fa328268239491d5045856356f02fba532ba963c
Nicolas Hervy <nicolas@hervy.se>
2021-11-24 16:10:32 +0100

    Input component now works.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,36 @@
-export const Input = () => {
+import { useRef } from 'react'
+import { Todo } from '../App'
+
+interface Props {
+  todos: Todo[]
+  setTodos: React.Dispatch<Todo[]>
+}
+
+export const Input = ({ todos, setTodos }: Props): JSX.Element => {
+  const todoNameRef = useRef<HTMLInputElement | null>(null)
+
+  function handleAddTodo() {
+    const inputTodo = todoNameRef.current?.value.trim()
+    if (inputTodo === '') return
+    const idArray = todos.map((todo) => todo.id)
+    const nextId =
+      idArray.reduce((previousValue, currentValue) =>
+        Math.max(previousValue, currentValue)
+      ) + 1
+    const newTodo = {
+      userId: 1,
+      id: nextId,
+      title: inputTodo,
+      completed: false,
+    } as Todo
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <input type='text' />
-      <button>Click Me!</button>
+      <input ref={todoNameRef} type='text' />
+      <button onClick={handleAddTodo}>+</button>
     </>
   )
 }
