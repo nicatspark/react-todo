bd8cdc5ef8b27e56cf072d67da3023f0f447e755
Nicolas Hervy <nicolas@hervy.se>
2021-11-24 16:10:12 +0100

    Todolist now works.
    Co-authored-by: Victoria Graf <victoria.graf@skf.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useState } from 'react'
 import TodoList from './components/TodoList'
+import { Input } from './components/Input'
+
+export interface Todo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+export type InitialState = Todo[]
+
+const initialState = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false,
+  },
+]
 
 const App = () => {
+  const [todos, setTodos] = useState(initialState)
+
   return (
     <div className='App'>
-      <TodoList />
+      <TodoList todos={todos} setTodos={setTodos} />
+      <Input todos={todos} setTodos={setTodos} />
     </div>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Todo } from '../App'
 
-const TodoList = () => {
+interface Props {
+  todos: Todo[]
+  setTodos: React.Dispatch<Todo[]>
+}
+
+const TodoList = ({ todos, setTodos }: Props): JSX.Element => {
+  const toggleTodo = (toggleId: number) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === toggleId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            className={todo.completed ? 'done' : undefined}
+            key={todo.id}
+            onClick={() => toggleTodo(todo.id)}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
